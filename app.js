6e82383c3c2c1ab9fb001ed8c83103616ca8b829
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-03-16 18:45:35 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Regform from './components/Regform';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+     <Regform/>
+     {/* <Login/> */}
+    </div>
+  );
+}
+
+export default App;
